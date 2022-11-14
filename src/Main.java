336963eaf96e88ef769e379d3a83f01f684b70d9
eva-lin <tagirmirzoeva@yandex.ru>
2022-11-14 16:33:58 +0300

    Урок 10. Сollections framework. Задание 1. Посчитать вхождения на слово «Страдание» (вне зависимости от склонения и регистра) в книге «Война и Мир том 1» и выложить в Git

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] words = null;
+        int count =0;
+        File f = new File("voina i mir 1 tom.txt");
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(f))){
+            String line = "";
+
+            while ((line = br.readLine())!= null){
+                lines.add(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Pattern pattern = Pattern.compile("[Сс]трад.*");
+        Matcher matcher =  pattern.matcher("");
+        if (matcher.matches()){
+            System.out.println("Yes");
+        }
+
+        for (String st: lines){
+            words = st.split(" ");
+            for (String word: words) {
+                matcher = pattern.matcher(word);
+                if (matcher.matches()) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Найдено "+ count + " повторений");
+    }
+}
