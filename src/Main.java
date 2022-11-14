f49560b665c67e7af0eb0ee418f1308676c1e1b3
eva-lin <tagirmirzoeva@yandex.ru>
2022-11-14 21:52:19 +0300

    Урок 10. Сollections framework. Задание 2. Сделать тоже самое с помощью класса Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         String[] words = null;
         int count =0;
+        String line = "";
         File f = new File("voina i mir 1 tom.txt");
         ArrayList<String> lines = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(f))){
-            String line = "";
-
-            while ((line = br.readLine())!= null){
+        Scanner scnr = new Scanner(f);
+            while (scnr.hasNextLine()){
+                line = scnr.nextLine();
                 lines.add(line);
             }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
         Pattern pattern = Pattern.compile("[Сс]трад.*");
         Matcher matcher =  pattern.matcher("");
         if (matcher.matches()){
